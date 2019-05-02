fcef3d683652ececf3a4b8b92914272805992a0f
Huiqin <kathryn.h.gao@gmail.com>
2019-05-02 10:24:51 +0800

    Add a Python file to randomly pick what to do the next. Update JavaScript progress

---
+++ whatNext.py
@@ -0,0 +1,11 @@
+import random
+
+list  = ['SQL', 'JavaScript', 'Git']
+
+print "\n"
+print ">>>>>>>>>>>>>>"
+print "\n"
+print "Now:    " + random.choice(list)
+print "\n"
+print ">>>>>>>>>>>>>>"
+print "\n"
